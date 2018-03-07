19463942e699e0226fcf8491a1a466534f8a8772
Saori Tawada <cktistwd@gmail.com>
2018-03-07 14:40:05 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/saori/tawada/taskapp/Task.java
@@ -0,0 +1,63 @@
+package jp.techacademy.saori.tawada.taskapp;
+
+/**
+ * Created by admin on 2018/03/05.
+ */
+
+import java.io.Serializable;
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Task extends RealmObject implements Serializable {
+    private String title;   //タイトル
+    private String contents;    //内容
+    private String category;    //カテゴリ
+    private Date date;  //日時
+
+
+    //id をプライマリーキーとして設定
+    @PrimaryKey
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
